4d346b3131ac8ed909f61f6bfdf48e25d7184fc0
Firat Kaya <firatkayaegitim@hotmail.com>
2026-05-21 01:24:59 +0300

    cleanup repo

--- telegram-test.js
+++ telegram-test.js
@@ -4,14 +4,20 @@ const TOKEN = "AAETlnMGp23VtDBVUkPbhjrlL7lAa3as6v0";
 
 const CHAT_ID = "5014444499";
 
-axios
-    .post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+axios.post(
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+    {
         chat_id: CHAT_ID,
-        text: "🚨 Terms Radar test bildirimi başarılı!"
-    })
-    .then(res => {
-        console.log("Mesaj gönderildi!");
-    })
-    .catch(err => {
-        console.log(err.response.data);
-    });
+        text: "🚨 Terms Radar test bildirimi başarılı 🚀"
+    }
+)
+.then(res => {
+
+    console.log("Mesaj gönderildi!");
+
+})
+.catch(err => {
+
+    console.log(err.response.data);
+
+});
